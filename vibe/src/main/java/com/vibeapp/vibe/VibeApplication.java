b959b2dd7e04ed1abbcc7d78cecaaa14b4453af3
Saarang <saaranganand2013@gmail.com>
2024-02-08 16:32:13 -0800

    maven init

---
+++ vibe/src/main/java/com/vibeapp/vibe/VibeApplication.java
@@ -0,0 +1,13 @@
+package com.vibeapp.vibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeApplication.class, args);
+	}
+
+}
---
+++ vibe/src/test/java/com/vibeapp/vibe/VibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibeapp.vibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
