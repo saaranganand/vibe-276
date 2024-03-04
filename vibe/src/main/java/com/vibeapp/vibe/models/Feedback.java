37f6842420990d64bdd3983db613c216aa62433c
Tommy Duong <tommyduong03@gmail.com>
2024-03-03 22:36:16 -0800

    Created feedback database values to store feedback messages

--- vibe/src/main/java/com/vibeapp/vibe/models/Feedback.java
+++ vibe/src/main/java/com/vibeapp/vibe/models/Feedback.java
@@ -1,5 +1,33 @@
 package com.vibeapp.vibe.models;
 
+import jakarta.persistence.*;
+
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Feedback(String message) {
+        this.message = message;
+    }
+
     
 }
