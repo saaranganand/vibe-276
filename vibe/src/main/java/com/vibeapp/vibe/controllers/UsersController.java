1c37ad5a9c285b104d95bc83e26743c72de95bf8
Saarang <saaranganand2013@gmail.com>
2024-03-07 21:44:02 -0800

    registration validation succesful

--- vibe/src/main/java/com/vibeapp/vibe/controllers/UsersController.java
+++ vibe/src/main/java/com/vibeapp/vibe/controllers/UsersController.java
@@ -14,7 +14,12 @@ import org.springframework.web.servlet.view.RedirectView;
 import com.vibeapp.vibe.models.User;
 import com.vibeapp.vibe.models.UserRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class UsersController {
@@ -27,14 +32,55 @@ public class UsersController {
     // return new RedirectView("login");
     // }
 
+    // @PostMapping("/users/register")
+    // public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+    //     System.out.println("ADD new user");
+    //     String newEmail = newuser.get("email");
+    //     String newName = newuser.get("username");
+    //     String newPassword = newuser.get("password");
+    //     if(!(newEmail.isEmpty() || newName.isEmpty() || newPassword.isEmpty() || (userRepo.findByEmail(newEmail) != null) || (userRepo.findByName(newName) != null)))
+    //     {
+    //         userRepo.save(new User(newName, newPassword, newEmail));
+    //         response.setStatus(201);
+    //         return "/users/registerSuccess";
+    //     }
+    //     else {
+    //         response.setStatus(201);
+    //         return "/users/registerFailed";
+    //     }
+    // }
+
     @PostMapping("/users/register")
     public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
         System.out.println("ADD new user");
         String newEmail = newuser.get("email");
         String newName = newuser.get("username");
         String newPassword = newuser.get("password");
-        userRepo.save(new User(newName, newPassword, newEmail));
-        response.setStatus(201);
-        return "/users/registerSuccess";
+
+        User existingUserByEmail = userRepo.findByEmail(newEmail);
+        User existingUserByUsername = userRepo.findByName(newName);
+    
+        if (existingUserByEmail != null || existingUserByUsername != null) {
+            response.setStatus(409);
+            return "/users/registerFailed";
+        }
+        else {
+            userRepo.save(new User(newName, newPassword, newEmail));
+            response.setStatus(201);
+            return "/users/registerSuccess";
+        }
     }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
+        User user = (User) session.getAttribute("session_user");
+        if (user != null) {
+            return "login";
+        }
+        else {
+            model.addAttribute("user", user);
+            return "users/protected";
+        }
+    }
+    
 }
--- vibe/src/main/resources/static/js/register.js
+++ vibe/src/main/resources/static/js/register.js
@@ -4,12 +4,10 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submitbtn', e => {
+form.addEventListener('submit', e => {
     e.preventDefault();
 
     validateInputs();
-
-    document.getElementById('registerform').submit();
 });
 
 const setError = (element, message) => {
@@ -41,39 +39,58 @@ const validateInputs = () => {
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
 
+    isValid = true;
+
     if(usernameValue === '') {
         setError(username, 'Username is required');
+        isValid = false;
     } else {
         setSuccess(username);
+        isValid = true;
     }
 
     if(emailValue === '') {
         setError(email, 'Email is required');
+        isValid = false;
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        isValid = false;
     } else {
         setSuccess(email);
+        isValid = true;
     }
 
     if(passwordValue === '') {
         setError(password, 'Password is required');
+        isValid = false;
     } else if (passwordValue.length < 8 ) {
         setError(password, 'Password must be at least 8 characters');
+        isValid = false;
     } else if (!/[a-z]/.test(passwordValue)) {
         setError(password, 'Password must contain at least one lowercase character');
+        isValid = false;
     } else if (!/[A-Z]/.test(passwordValue)) {
         setError(password, 'Password must contain at least one uppercase character');
+        isValid = false;
     } else if (!/[0-9]/.test(passwordValue)) {
         setError(password, 'Password must contain at least one number');
+        isValid = false;
     } else {
         setSuccess(password);
+        isValid = true;
     }
 
     if(password2Value === '') {
         setError(password2, 'Please confirm your password');
+        isValid = false;
     } else if (password2Value !== passwordValue) {
         setError(password2, "Passwords do not match");
+        isValid = false;
     } else {
         setSuccess(password2);
+        isValid = true;
     }
+
+    if(isValid === true)
+        document.getElementById('registerform').submit();
 };
