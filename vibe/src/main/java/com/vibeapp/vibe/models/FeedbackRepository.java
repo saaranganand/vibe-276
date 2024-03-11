bd0cd7f98ead1694c4717abad0dd258e908fcb7c
Tommy Duong <tommyduong03@gmail.com>
2024-03-10 21:09:29 -0700

    Finished functionality and style

--- vibe/src/main/java/com/vibeapp/vibe/models/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vibeapp.vibe.models;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-    
-} 
