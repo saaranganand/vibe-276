86e12ba3a7527e37f6ba37330158737b258b946e
csooyeonii <127283946+csooyeonii@users.noreply.github.com>
2024-03-11 14:38:45 -0700

    explore page

--- vibe/src/main/java/com/vibeapp/vibe/models/MusiciansRepository.java
+++ vibe/src/main/java/com/vibeapp/vibe/models/MusiciansRepository.java
@@ -1,9 +1,9 @@
 package com.vibeapp.vibe.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MusiciansRepository extends JpaRepository<Musician, Integer>{
+    List<Musician> findByUsernameContainingIgnoreCase(String input);
 
-    
-    
 }
