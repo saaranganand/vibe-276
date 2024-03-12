fee0bdb4d0c821b36689f8c3977661b1aab92ca1
Tommy Duong <tommyduong03@gmail.com>
2024-03-11 21:44:20 -0700

    implemented feedback functonality

--- vibe/src/main/java/com/vibeapp/vibe/VibeApplication.java
+++ vibe/src/main/java/com/vibeapp/vibe/VibeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VibeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VibeApplication.class, args);
 	}
