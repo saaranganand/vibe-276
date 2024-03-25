6d439042742abd3ea8ac7c5a6bd96a2887ed069f
allengao04 <151907792+allengao04@users.noreply.github.com>
2024-03-25 16:17:22 -0700

    Update ProfileController.java
    Change endpoints to homelogged in, command out test case

--- vibe/src/main/java/com/vibeapp/vibe/controllers/ProfileController.java
+++ vibe/src/main/java/com/vibeapp/vibe/controllers/ProfileController.java
@@ -55,28 +55,28 @@ public class ProfileController{
         }
 
         Profilerepo.save(new Profile(newName, newCityName, newInstrument, newAge,newskilllevel,newTop1artist, newTop2artist, newTop3artist, newGenres, host,imageBytes));
-        return "/userimage";
+        return "users/home-loggedin";
     }
 
-    @GetMapping("/userimage")
-    public String getimage(Model model){
-        List<Profile> profiles = Profilerepo.findAll();
-        model.addAttribute("check", profiles);
-        return "users/imagetest";
+    // @GetMapping("/userimage")
+    // public String getimage(Model model){
+    //     List<Profile> profiles = Profilerepo.findAll();
+    //     model.addAttribute("check", profiles);
+    //     return "users/imagetest";
 
-    }
-    @GetMapping("/user/image/{userId}")
-    public ResponseEntity<byte[]> getUserImage(@PathVariable int userId) {
-        Profile profile = Profilerepo.findById(userId).orElse(null);
-        if (profile != null && profile.getImage() != null) {
-            return ResponseEntity
-                    .ok()
-                    .contentType(MediaType.IMAGE_JPEG) // Adjust based on your image type
-                    .body(profile.getImage());
-        } else {
-            // Optionally, return a default image if the user has no image
-            return ResponseEntity.notFound().build();
-        }
-    }
+    // }
+    // @GetMapping("/user/image/{userId}")
+    // public ResponseEntity<byte[]> getUserImage(@PathVariable int userId) {
+    //     Profile profile = Profilerepo.findById(userId).orElse(null);
+    //     if (profile != null && profile.getImage() != null) {
+    //         return ResponseEntity
+    //                 .ok()
+    //                 .contentType(MediaType.IMAGE_JPEG) // Adjust based on your image type
+    //                 .body(profile.getImage());
+    //     } else {
+    //         // Optionally, return a default image if the user has no image
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
 
 }
