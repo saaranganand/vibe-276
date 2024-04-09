9a7b26f74f64f9e0dc46d1956b71684f4d6546dc
Saarang Anand <saaranganand2013@gmail.com>
2024-04-09 00:08:27 -0700

    filter announcements by latest or oldest

--- vibe/src/main/java/com/vibeapp/vibe/models/AnnouncementRepository.java
+++ vibe/src/main/java/com/vibeapp/vibe/models/AnnouncementRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer>{
     List<Announcement> findAllByOrderByAidDesc();
+    List<Announcement> findAllByOrderByAidAsc();
     Announcement findByAid(int aid);
 }
